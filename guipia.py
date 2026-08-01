796636d8117dbe68fbdb6dc8fbebeb6cfd50c7a5
wskakuj <nie.pyszcz@gmail.com>
2026-08-01 17:32:05 +0200

    Add files via upload

--- guipia.py
+++ guipia.py
@@ -43,7 +43,7 @@ except ImportError:
     )
 
 # --- KONFIGURACJA AKTUALIZACJI GITHUB ---
-CURRENT_VERSION = "v1.4.1"
+CURRENT_VERSION = "v1.4.3"
 GITHUB_USER = "wskakuj"
 GITHUB_REPO = "kombajn-lesny-pro"
 
